f5b5ab98d913cbcead89f9d03f573788c6ce354c
JunlingZhuang <108850976+JunlingZhuang@users.noreply.github.com>
2023-02-05 11:23:23 -0500

    update Assignment1

---
+++ starlink/src/constants.js
@@ -0,0 +1,7 @@
+export const SAT_API_KEY = "LZR5J5-99V74J-8TCUGY-4ZL6";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "http://localhost:3000/"; // your backend endpoint when deploy
